603b4e1a4f0dd2f6290685f9eb6963c4ef44b286
rogetsun <songyw163@163.com>
2018-06-29 15:13:40 +0800

    initial commit

--- src/main/java/com/uv/spring/aop/aspect/MyAspect.java
+++ src/main/java/com/uv/spring/aop/aspect/MyAspect.java
@@ -13,14 +13,15 @@ import java.util.Arrays;
 public class MyAspect {
     private void before(JoinPoint jp) {
 
+        System.out.println("**********MyAspect.before********");
         System.out.println(jp);
-
+        System.out.println(this);
+        System.out.println(this.hashCode());
         //连接点方法即为被切面植入的目标方法
         System.out.println("获取连接点方法运行时的入参列表:" + Arrays.toString(jp.getArgs()));
         System.out.println("获取连接点的方法签名对象:" + jp.getSignature());
-        System.out.println("获取连接点所在的目标对象:" + jp.getTarget());
-        System.out.println("获取代理对象本身:" + jp.getThis());
-
+        System.out.println("获取连接点所在的目标对象:" + jp.getTarget() + ", hashCode:" + jp.getTarget().hashCode());
+        System.out.println("获取代理对象本身:" + jp.getThis() + ", hashCode:" + jp.getThis().hashCode());
 
         MethodSignature signature = (MethodSignature) jp.getSignature();
         System.out.println("获取连接点方法:" + signature.getMethod());
@@ -28,13 +29,16 @@ public class MyAspect {
         //连接点方法上的注解
         Limit l = m.getAnnotation(Limit.class);
         System.out.println(l);
+        System.out.println("**********MyAspect.before********");
 
     }
 
     private void after(JoinPoint joinPoint) {
+        System.out.println("**********MyAspect.after********");
 
         System.out.println(joinPoint);
         System.out.println(this);
+        System.out.println("**********MyAspect.after********");
     }
 
 }
