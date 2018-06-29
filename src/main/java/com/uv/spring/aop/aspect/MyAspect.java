f678afb97bd9c218c48b3efb2fffdd4aa1bcc79d
rogetsun <songyw163@163.com>
2018-06-29 15:29:48 +0800

    initial commit

--- src/main/java/com/uv/spring/aop/aspect/MyAspect.java
+++ src/main/java/com/uv/spring/aop/aspect/MyAspect.java
@@ -15,14 +15,8 @@ public class MyAspect {
     private void before(JoinPoint jp) {
 
         System.out.println("**********MyAspect.before********");
+
         System.out.println(jp);
-        System.out.println(this);
-        System.out.println(this.hashCode());
-        //连接点方法即为被切面植入的目标方法
-        System.out.println("获取连接点方法运行时的入参列表:" + Arrays.toString(jp.getArgs()));
-        System.out.println("获取连接点的方法签名对象:" + jp.getSignature());
-        System.out.println("获取连接点所在的目标对象(原对象):" + jp.getTarget() + ", hashCode:" + jp.getTarget().hashCode() + ", isAopProxy:" + AopUtils.isAopProxy(jp.getTarget()));
-        System.out.println("获取代理对象本身:" + jp.getThis() + ", hashCode:" + jp.getThis().hashCode() + ", isAopProxy:" + AopUtils.isAopProxy(jp.getThis()));
 
         MethodSignature signature = (MethodSignature) jp.getSignature();
         Method m = signature.getMethod();
@@ -30,6 +24,14 @@ public class MyAspect {
         //连接点方法上的注解
         Limit l = m.getAnnotation(Limit.class);
         System.out.println(l);
+
+        //连接点方法即为被切面植入的目标方法
+        System.out.println("获取连接点方法运行时的入参列表:" + Arrays.toString(jp.getArgs()));
+        System.out.println("获取连接点的方法签名对象:" + jp.getSignature());
+        System.out.println("获取连接点所在的目标对象(原对象):" + jp.getTarget() + ", hashCode:" + jp.getTarget().hashCode() + ", isAopProxy:" + AopUtils.isAopProxy(jp.getTarget()));
+        System.out.println("获取代理对象本身:" + jp.getThis() + ", hashCode:" + jp.getThis().hashCode() + ", isAopProxy:" + AopUtils.isAopProxy(jp.getThis()));
+
+
         System.out.println("**********MyAspect.before********");
 
     }
