080176477e7bdaf7604a187d8a956d31d988bc2a
rogetsun <songyw163@163.com>
2018-07-02 17:00:51 +0800

    initial commit

--- src/main/java/com/uv/spring/aoparound/MyAspect.java
+++ src/main/java/com/uv/spring/aoparound/MyAspect.java
@@ -2,6 +2,9 @@ package com.uv.spring.aoparound;
 
 import com.uv.spring.aop.annotation.Limit;
 import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.reflect.MethodSignature;
+
+import java.lang.reflect.Method;
 
 /**
  * @author uvsun 2018/7/2 下午3:10
@@ -11,24 +14,21 @@ public class MyAspect {
     public Object around(ProceedingJoinPoint jp, Limit limit, String name) {
 
         try {
-            System.out.println("Around begin");
+            System.out.println("********* around2 begin ********");
             System.out.println(limit);
             System.out.println(name);
+
+            Method m = ((MethodSignature) jp.getSignature()).getMethod();
+            System.out.println(m.getReturnType());
+            System.out.println(m.getReturnType().equals(Void.TYPE));
             Object r = jp.proceed();
             System.out.println("proceed return:" + r);
-            System.out.println("around end");
+            System.out.println("********* around2 end ********");
             return r;
         } catch (Throwable throwable) {
             throwable.printStackTrace();
         }
         return null;
-    }
-
-    public Object around2(ProceedingJoinPoint jp) throws Throwable {
-        System.out.println("********* around2 begin ********");
-        Object r = jp.proceed();
-        System.out.println("********* around2 end ********");
-        return r;
     }
 
 
